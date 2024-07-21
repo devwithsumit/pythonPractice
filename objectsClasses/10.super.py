817028fc1882bf9f65801291774656d81e062d75
webwithsumit <sumit2410kushwaha@gmail.com>
2024-07-22 00:04:30 +0530

    static and super

--- objectsClasses/10.super.py
+++ objectsClasses/10.super.py
@@ -1,5 +1,5 @@
-#The super() keyword in Python is used to refer to the
-# parent class. It is especially useful when a class 
+# The super() keyword in Python is used to refer to the
+# parent class. It is especially useful when a class
 # inherits from multiple parent classes and you want
 # to call a method from one of the parent classes.
 # class parent:
@@ -9,20 +9,23 @@
 #     def childMethod(self):
 #         print('child method')
 #         super().parentMethod() #calling parent's method inside child's method
-        
+
 # obj = child()
 # obj.childMethod()
 
 
 class Employee:
-  def __init__(self, name, id):
-    self.name = name
-    self.id = id
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
 
 class Programmer(Employee):
-    def __init__(self,name,id,lang):
-        super().__init__(name,id) #calling parent's construction function
+    def __init__(self, name, id, lang):
+        super().__init__(name, id)  # calling parent's constructor function
         self.lang = lang
+
+
 rohan = Employee("Rohan Das", "420")
 print(rohan.__dict__)
 Sumit = Programmer("Sumit", "2345", "Python")
--- objectsClasses/6.staticMethod.py
+++ objectsClasses/6.staticMethod.py
@@ -4,12 +4,17 @@ class math:
     def addnum(self,n): #normal method
         print(f'adding {self.num} to {n}')
         self.num = self.num + n
+        return self.num
     #static method doesn't require self parameter
     @staticmethod
     def add(a,b): 
         return a + b
-result = math.add(1,6) #can be accessed directly using classname.methodName(a,b)
-print(result)
+    
+result1 = math(8) #cannot acces math.addnum directly
+print(result1.addnum(5))
+
+result2 = math.add(1,6) #can be accessed directly using classname.methodName(a,b)
+print(result2)
 
 # a = math(1)
 # print(a.num)
