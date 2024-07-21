11c8d94df98e01ad3880ac1cf9958eee470139fe
webwithsumit <sumit2410kushwaha@gmail.com>
2024-07-21 23:47:30 +0530

    Upside down Triangle pattern with 2 line

---
+++ clg/demo.py
@@ -0,0 +1,34 @@
+# for i in range(4, 0,-1):
+#     for j in range(0, i):
+#         print("* ", end="")
+#     print()
+#     for k in range(5 - i):
+#         print(" ", end="")
+
+
+# for i in range(4, 0, -1):
+#     print(" " * (4 - i) + "* " * i)
+    
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+for i in range(4,0,-1):
+      print("* " * i)
+      
