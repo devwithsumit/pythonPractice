3201c6981f2fb97f95e249a44982df6c13384d49
webwithsumit <sumit2410kushwaha@gmail.com>
2024-07-21 23:46:38 +0530

    if else short hand trick

--- basics/ifelseShortHand.py
+++ basics/ifelseShortHand.py
@@ -6,4 +6,4 @@ print("A") if a > b else print("=") if a == b else print("B")
 c = a if a > b else b  # most useful case
 print(c)
 
-max(input("1st number")max(input("2nd number")max(input("3rd number")max(input("4th number")))))
+# print(max(input("1st number"),max(input("2nd number"),max(input("3rd number"),max(input("4th number"))))))
